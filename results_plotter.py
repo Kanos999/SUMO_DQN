f2e48a3bd2815ae3c2d6971d3c2c7d5d16da38ba
Kane Jackson <kane@kanejackson.com>
2024-11-14 17:43:28 +1100

    updated plot script

--- results_plotter.py
+++ results_plotter.py
@@ -3,8 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Directories where episode CSV files for both sets are stored
-directory1 = 'outputs/intersection_DoubleDQN'
-directory2 = 'outputs/intersection'
+directory1 = 'outputs/baseline-1'
+directory2 = 'outputs/baseline-2'
+directory3 = 'outputs/'
 
 # Initialize lists to store the average waiting time per episode for both sets
 average_waiting_times_1 = []
@@ -28,10 +29,16 @@ def calculate_average_waiting_times(directory):
 average_waiting_times_1 = calculate_average_waiting_times(directory1)
 average_waiting_times_2 = calculate_average_waiting_times(directory2)
 
+
+data = pd.read_csv('outputs/simulation_results_wait_time.csv')
+average_waiting_times_3 = data['system_total_waiting_time']
+
+
 # Plot the results
 plt.figure(figsize=(10, 6))
-plt.plot(average_waiting_times_1, marker='o', linestyle='-', label='DoubleDQN')
-plt.plot(average_waiting_times_2, marker='x', linestyle='--', label='DQN')
+plt.plot(average_waiting_times_1, marker='o', linestyle='-', label='DQN')
+plt.plot(average_waiting_times_2, marker='o', linestyle='-', label='Double DQN')
+plt.plot(average_waiting_times_3, marker='o', linestyle='-', label='Our model')
 plt.xlabel('Episode')
 plt.ylabel('Average System Total Waiting Time')
 plt.title('Average System Total Waiting Time Across Episodes')
